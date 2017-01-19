35eea597c819a3b9ed1c1ac43f0d301cafe0a9bc
garyuppal <gary.s.uppal@gmail.com>
2017-01-19 13:58:54 -0500

    modified Simulator.cpp

--- Simulator.cpp
+++ Simulator.cpp
@@ -1346,15 +1346,16 @@ inline void Simulator::bacteriaUpdate() {
 }
 
 inline void Simulator::updateFields() {
-/*
+
   // Diffusion and decay of resource field
   delSqr(resource, buffer); 
   for (int y=0; y<resource.getDY(); y++)
     for (int x=0; x<resource.getDX(); x++) {
       resource.at(x,y) += epsilon*(resourceDiffusion*buffer.at(x,y) + replenish);
-      resource.at(x,y) -= epsilon*lamR*resource.at(x,y); // decay or resource
+  /* 
+   resource.at(x,y) -= epsilon*lamR*resource.at(x,y); // decay or resource
   // advection:
-  vect<> velocity;
+  vect<> velocity = {0.0,0.0};
   vect<> pos = {x, y};
   if (flowFunc) velocity = flowFunc(pos);
   double velX = velocity.x;
@@ -1364,19 +1365,20 @@ inline void Simulator::updateFields() {
   double gfx = gradfield.x;
   double gfy = gradfield.y;
   resource.at(x,y) += epsilon*(velX*gfx + velY*gfy);
- 
+*/
      resource.at(x,y) = resource.at(x,y)<0 ? 0 : resource.at(x,y);
     }
-  */
+  
   
   // Diffusion of waste field
   delSqr(waste, buffer);
   for (int y=0;y<waste.getDY(); y++)
     for(int x=0; x<waste.getDX(); x++) {
       waste.at(x,y) += epsilon*(wasteDiffusion*buffer.at(x,y) + wasteSource);
-      waste.at(x,y) -= epsilon*lamW*waste.at(x,y);  // decay of waste
+/*  
+    waste.at(x,y) -= epsilon*lamW*waste.at(x,y);  // decay of waste
   // advection:
-  vect<> velocity;
+  vect<> velocity = {0.0,0.0};
   vect<> pos = {x, y};
   if (flowFunc) velocity = flowFunc(pos);
   double velX = velocity.x;
@@ -1386,7 +1388,7 @@ inline void Simulator::updateFields() {
   double gfx = gradfield.x;
   double gfy = gradfield.y;
   waste.at(x,y) += epsilon*(velX*gfx + velY*gfy);
- 
+*/ 
       waste.at(x,y) = waste.at(x,y)<0 ? 0 : waste.at(x,y);
     }
 
