63ca97792ee56a7188ab9c5441464585caa25ecb
nrupprecht <nathaniel.rupprecht@gmail.com>
2018-10-18 14:51:34 -0400

    Hopefully corrected most of the simd errors I was getting on the crc

--- GFlowSim/src/utility/simd_types.hpp
+++ GFlowSim/src/utility/simd_types.hpp
@@ -14,6 +14,17 @@ const int simd_valid = 0xffffffff;
   // What type we use as the vector of integers
   typedef int simd_int;
 
+  // Logic
+  inline simd_int simd_and(const simd_int a, const simd_int b) { return a & b; }
+
+  // Casting
+  inline simd_int   simd_cast_int(const simd_float a) { return reinterpret_cast<int>(a); }
+  inline simd_float simd_cast_float(const simd_int a) { return reinterpret_cast<float>(a); }
+  inline void move_to_int(const simd_float source, simd_int& dest) { dest = static_cast<int>(source[i]); }
+
+  // Masking 
+  inline simd_float simd_mask(const simd_float a, const simd_float m) { return simd_cast_float(simd_and(simd_cast_int(a), simd_cast_int(m))); }
+
   // Setting, loading, storing
   inline simd_float simd_load (const float *a)                   { return *a; }
   inline simd_float simd_load_u(const float *a)                  { return *a; }
@@ -46,7 +57,7 @@ const int simd_valid = 0xffffffff;
   typedef __m128i simd_int;
 
   // Constants
-  const __m128 SIGNMASK = _mm_castsi128_ps(_mm_set1_epi32(0x80000000));
+  const simd_float SIGNMASK = _mm_castsi128_ps(_mm_set1_epi32(0x80000000));
 
   // Logic
   inline simd_int simd_and(const simd_int a, const simd_int b) { return _mm_and_si128(a, b); }
@@ -117,22 +128,48 @@ const int simd_valid = 0xffffffff;
   // What type we use as the vector of integers
   typedef __m256i simd_int;
 
+  // Constants
+  const simd_float SIGNMASK = _mm256_castsi256_ps(_mm256_set1_epi32(0x80000000));
+
+  // Logic
+  inline simd_int simd_and(const simd_int a, const simd_int b) { return _mm256_and_si256(a, b); }
+
+  // Casting
+  inline simd_int   simd_cast_int(const simd_float a) { return _mm256_castps_si256(a); }
+  inline simd_float simd_cast_float(const simd_int a) { return _mm256_castsi256_ps(a); }
+
+  inline void move_to_int(const simd_float source, simd_int& dest) {
+    // This is done serially.
+    for (int i=0; i<simd_data_size; ++i)
+      reinterpret_cast<int32_t*>(&dest)[i] = static_cast<int>(source[i]);
+  }
+
+  // Masking 
+  inline simd_float simd_mask(const simd_float a, const simd_float m) { return simd_cast_float(simd_and(simd_cast_int(a), simd_cast_int(m))); }
+
   // Setting, loading, storing
-  inline simd_float simd_load (const float *a)                   { return _mm256_load_ps(a); }
-  inline simd_float simd_load_u(const float *a)                  { return _mm256_loadu_ps(a); }
+  inline simd_float simd_load (const float *a)                         { return _mm256_load_ps(a); }
+  inline simd_float simd_load_u(const float *a)                        { return _mm256_loadu_ps(a); }
   inline simd_float simd_set1 (const float a)                          { return _mm256_set1_ps(a); }
+  inline simd_int   simd_set1_int(const int a)                         { return _mm256_set1_epi32(a); }
   inline simd_float simd_set_zero ()                                   { return _mm256_setzero_ps(); }
   inline void       simd_store(const simd_float src, float *dest)      { _mm256_store_ps(dest, src); }
-//  inline void       simd_store(const simd_int src, int *dest)          { _mm256_store_si256(dest, src); }
   inline void       simd_store_u(const simd_float src, float *dest)    { _mm256_storeu_ps(dest, src); }
-//  inline void       simd_store_u(const simd_int src, int *dest)        { _mm256_storeu_si256(dest, src); }
-   // Arithmetic
+  // Arithmetic
   inline simd_float simd_add  (const simd_float a, const simd_float b) { return _mm256_add_ps(a, b); }
   inline simd_float simd_sub  (const simd_float a, const simd_float b) { return _mm256_sub_ps(a, b); }
   inline simd_float simd_mult (const simd_float a, const simd_float b) { return _mm256_mul_ps(a, b); }
-  inline void    simd_plus_eq (simd_float &a, const simd_float b)      { a = _mm256_add_ps(a, b); }
+  inline simd_float simd_div  (const simd_float a, const simd_float b) { return _mm256_div_ps(a, b); }
+  inline void       simd_plus_eq (simd_float &a, const simd_float b)   { a = _mm256_add_ps(a, b); }
+  inline simd_float simd_sqrt(const simd_float a)                      { return _mm256_sqrt_ps(a); }
+  inline simd_float simd_inv_sqrt(const simd_float a)                  { return _mm256_rsqrt_ps(a); }
+  inline simd_float simd_abs(const simd_float a)                       { return _mm256_andnot_ps(SIGNMASK, a); }
+  inline simd_float simd_negate(const simd_float a)                    { return _mm256_xor_ps(a, SIGNMASK); }
+  
   // Comparisons
-  // inline simd_int simd_less_than(const simd_float a, const simd_float b) { return _mm256_castpd_si256(_mm256_cmp_pd(a, b, _CMP_LT_OS)); }
+  inline simd_float simd_less_than(const simd_float a, const simd_float b) { return _mm256_cmp_ps(a, b, 1); }
+  inline simd_float simd_clamp(const simd_float a) { return simd_mask(a, simd_less_than(simd_set1(0.), a)); }
+  inline simd_float simd_un_clamp(const simd_float a) { return simd_mask(a, simd_less_than(a, simd_set1(0.))); }
 
   // Special load
   template<int dimensions> inline simd_float simd_load_constant(const float *a, const int i) {
@@ -162,9 +199,6 @@ const int simd_valid = 0xffffffff;
     return simd_set1(a[i/8]);
   }
 
-  // Constants
-  const simd_float minus_one = simd_set1(-1.);
-
 #elif SIMD_TYPE==SIMD_MIC
   // The number of floats per vector
   const int simd_data_size = 16u;
@@ -172,6 +206,22 @@ const int simd_valid = 0xffffffff;
   typedef __m512d simd_float;
   // What type we use as the vector of integers
   typedef __m512i simd_int;
+
+  // Logic
+  inline simd_int simd_and(const simd_int a, const simd_int b) { return _mm512_and_si128(a, b); }
+
+  // Casting
+  inline simd_int   simd_cast_int(const simd_float a) { return _mm512_castps_si128(a); }
+  inline simd_float simd_cast_float(const simd_int a) { return _mm512_castsi128_ps(a); }
+
+  inline void move_to_int(const simd_float source, simd_int& dest) {
+    // This is done serially.
+    for (int i=0; i<simd_data_size; ++i)
+      reinterpret_cast<int32_t*>(&dest)[i] = static_cast<int>(source[i]);
+  }
+
+  // Masking 
+  inline simd_float simd_mask(const simd_float a, const simd_float m) { return simd_cast_float(simd_and(simd_cast_int(a), simd_cast_int(m))); }
 
   // Setting, loading, storing
   inline simd_float simd_load (const float const *a)                   { return _mm512_load_ps(a); }
