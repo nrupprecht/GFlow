44edd78b1593402b692890c7a0f17fdeca13a729
nrupprecht <nathaniel.rupprecht@gmail.com>
2019-11-16 18:23:50 -0500

    Updated config files, change to windtunnel modifier.

--- GFlowSim/src/modifiers/windtunnel.cpp
+++ GFlowSim/src/modifiers/windtunnel.cpp
@@ -1,22 +1,47 @@
 #include "windtunnel.hpp"
 // Other files
 #include "../base/simdata.hpp"
+#include "../base/topology.hpp"
+#include "../utility/vec.hpp"
 
 namespace GFlowSimulation {
 
-  WindTunnel::WindTunnel(GFlow *gflow, RealType v) : Modifier(gflow), velocity(v), halfWidth(2.), acceleration(2.) {
-    leftBound  = gflow->getBounds().min[0] + halfWidth;
-    rightBound = gflow->getBounds().max[0] - halfWidth;
+  WindTunnel::WindTunnel(GFlow *gflow, RealType v) : Modifier(gflow), velocity(v), halfWidth(5.), acceleration(2.) {
+    RealType min_x = gflow->getBounds().min[0], max_x = gflow->getBounds().max[0];
+    leftBound  = max(min(min_x + halfWidth, static_cast<RealType>(0.25*min_x)), min_x);
+    rightBound = min(max(max_x - halfWidth, static_cast<RealType>(0.75*max_x)), max_x);
   }
 
   void WindTunnel::post_forces() {
-    int size = simData->size();
+    // Check whether this processor overlaps with the acceleration zone.
+    auto &bnds = topology->getProcessBounds();
+    if (leftBound<=bnds.min[0] && bnds.max[0]<=rightBound) return;
 
-    RealType *vel = new RealType[sim_dimensions], *dv = new RealType[sim_dimensions];
-    zeroVec(vel, sim_dimensions); 
+    int size = simData->size_owned();
+    Vec vel(sim_dimensions), v2(0);
     vel[0] = velocity;
-    
-    RealType *x = simData->X_arr(), **v = simData->V(), **f = simData->F(), *im = simData->Im();
+    auto x = simData->X();
+    auto v = simData->V();
+    auto f = simData->F();
+    auto im = simData->Im();
+
+    for (int i=0; i<size; ++i) {
+      if (im[i]>0 && (x[i][0]<leftBound || rightBound<x[i][0])) {
+	// Wrap particle velocity.
+	v2.wrap(v[i], sim_dimensions);
+	// Find relative velocity.
+	vel -= v2;
+	vel *= acceleration*(1.f/im[i]);
+	plusEqVec(f[i], vel.data, sim_dimensions);
+	// Unwrap velocity.
+	v2.unwrap();
+	// Reset vel.
+	vel.zero();
+	vel[0] = velocity;
+      }
+    }
+
+    /*
     for (int i=0, j=0; i<size*sim_dimensions; i+=sim_dimensions, ++j) {
       if ((rightBound<x[i] || x[i]<leftBound) && im[j]>0) {
         subtractVec(vel, v[j], dv, sim_dimensions);
@@ -24,10 +49,7 @@ namespace GFlowSimulation {
         plusEqVec(f[j], dv, sim_dimensions);
       }
     }
-
-    // Clean up
-    delete [] vel;
-    delete [] dv;
+    */
   }
 
 }
